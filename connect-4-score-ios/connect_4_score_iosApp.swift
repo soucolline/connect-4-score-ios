52dfb4a8d615e6e49d853792cfb6b8dc6cd446af
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2024-06-22 23:11:08 +0200

    feat(master): initial commit

---
+++ connect-4-score-ios/connect_4_score_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  connect_4_score_iosApp.swift
+//  connect-4-score-ios
+//
+//  Created by Thomas Guilleminot on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct connect_4_score_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
